59ef3b40b1a96ac7c9329ba87f748ae09c5c9efa
aaaraptanov <aaaraptanov@skbkontur.ru>
2021-12-16 13:52:41 +0500

    day 16 part II

--- day16/day16.py
+++ day16/day16.py
@@ -1,7 +1,21 @@
+import numpy
+
 input_file = open('input.txt', mode='r')
 
-data = [bin(int(my_hexdata.strip(), 16))[2:].zfill(len(my_hexdata.strip()) * 4) for my_hexdata in input_file.readlines()]
-print(data)
+hexadecimals = [bin(int(i.strip(), 16))[2:].zfill(len(i.strip()) * 4)
+                # can read multiple lines (good for testing different cases at once)
+                for i in input_file.readlines()]
+
+operators = [
+    sum,
+    numpy.prod,
+    min,
+    max,
+    lambda x: x,
+    lambda x: 1 if x[0] > x[1] else 0,
+    lambda x: 1 if x[0] < x[1] else 0,
+    lambda x: 1 if x[0] == x[1] else 0
+]
 
 
 def part1(binary_num, version=None, type_id=None, expected_packets=1, recursion_level=0):
@@ -14,14 +28,11 @@ def part1(binary_num, version=None, type_id=None, expected_packets=1, recursion_
         elif type_id is None:
             type_id = int(binary_num[0:3], 2)
             binary_num = binary_num[3:]
-            print('\t' * recursion_level, "version", version)
-            print('\t' * recursion_level, "type_id", type_id)
         else:
             if type_id == 4:
                 b_num = binary_num[0:5]
                 binary_num = binary_num[5:]
 
-                print('\t' * recursion_level, "literal", int(b_num[1:], 2))
                 if b_num[0] == '0':
                     expected_packets -= 1
                     version = None
@@ -46,8 +57,55 @@ def part1(binary_num, version=None, type_id=None, expected_packets=1, recursion_
                 version = None
                 type_id = None
                 expected_packets -= 1
-    return (binary_num, version_sum)
+    return binary_num, version_sum
 
 
-for d in data:
-    print(part1(d))
+def part2(binary_num, version=None, type_id=None, recursion_level=0):
+    package_results = []
+    package_literal_b = ''
+    while len(binary_num) > 0:
+        if version is None:
+            version = int(binary_num[0:3], 2)
+            binary_num = binary_num[3:]
+            continue
+        if type_id is None:
+            type_id = int(binary_num[0:3], 2)
+            binary_num = binary_num[3:]
+            print('\n', '\t' * recursion_level, "version", version)
+            print('\t' * recursion_level, "type_id", type_id)
+            continue
+
+        if type_id == 4:
+            b_num = binary_num[0:5]
+            binary_num = binary_num[5:]
+
+            package_literal_b += b_num[1:]
+            if b_num[0] == '1':
+                continue
+            if b_num[0] == '0':
+                print('\t' * recursion_level, "literal", int(package_literal_b, 2))
+                return binary_num, int(package_literal_b, 2)
+        else:
+            i = binary_num[0]
+            binary_num = binary_num[1:]
+            if i == '0':
+                subpacket_l = binary_num[:15]
+                binary_num = binary_num[15:]
+                subpacket = binary_num[:int(subpacket_l, 2)]
+                binary_num = binary_num[int(subpacket_l, 2):]
+                while subpacket:
+                    (subpacket, result) = part2(subpacket, recursion_level=recursion_level + 1)
+                    package_results.append(result)
+            elif i == '1':
+                subpacket_l = int(binary_num[:11], 2)
+                binary_num = binary_num[11:]
+                for it in range(subpacket_l):
+                    binary_num, result = part2(binary_num, recursion_level=recursion_level + 1)
+                    package_results.append(result)
+            return binary_num, operators[type_id](package_results)
+
+
+for v in hexadecimals:
+    answer = [part1(v), part2(v)]
+    print("part 1:", answer[0][1])
+    print("part 2:", answer[1][1])
